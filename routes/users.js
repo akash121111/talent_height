4703877d851085025ab489e2518df67687f343ab
akash121111 <akashkumar121111@gmail.com>
2021-03-04 05:54:55 -0500

    get user change route

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ const User = require('../models/user');
 const router = express.Router();
 
 //get users
-router.get('/', async (req, res)=>{
+router.get('/abc', async (req, res)=>{
     try{
         const users = await User.find();
         res.json(users);
