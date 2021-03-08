63868a1f20c768aab040bf5e17c9da7a237f23e1
akash121111 <akashkumar121111@gmail.com>
2021-03-08 07:30:10 -0500

    export data from moongosse to excel sheet

--- routes/excels.js
+++ routes/excels.js
@@ -8,6 +8,65 @@
     var xlsxtojson = require("xlsx-to-json-lc");
     const User = require('../models/user');
     app.use(bodyParser.json());
+    const fs = require('fs');
+    const MongoClient = require('mongodb').MongoClient;
+    const excel = require('exceljs');
+    const url='mongodb+srv://talent:kamal1234@cluster0.walu6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
+
+// //export data from database
+
+
+
+
+//  /** API path that will export the files */
+ router.get('/export', function(req, res) {
+  
+// Create a connection to the MongoDB database
+MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
+    if (err) throw err;
+    
+    let dbo = db.db("myFirstDatabase");
+    
+    dbo.collection("users").find({}).toArray(function(err, result) {
+      if (err) throw err;
+      console.log(result);
+      
+      
+      let workbook = new excel.Workbook(); //creating workbook
+      let worksheet = workbook.addWorksheet('users'); //creating worksheet
+  
+      //  WorkSheet Header
+      worksheet.columns = [
+          { header: '_id', key: '_id', width: 10 },
+          { header: 'role', key: 'role', width: 30 },
+          { header: 'username', key: 'username', width: 30 },
+          { header: 'email', key: 'email', width: 30},
+          { header: 'password', key: 'password', width: 150},
+          { header: 'token', key: 'token', width: 150}
+      ];
+      
+      // Add Array Rows
+      worksheet.addRows(result);
+      
+      // Write to File
+      workbook.xlsx.writeFile("users.xlsx")
+          .then(function() {
+              console.log("file saved!");
+          });
+      
+      db.close();
+    });
+  });
+  
+});
+
+
+
+
+
+
+    //load data to database
 
     function loaddata(data) {
 
